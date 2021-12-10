2b27bff439833384a1f06f273e30bbcfec5ffc79
Oriol Gonzalo <ogonzalo@uoc.edu>
2021-12-10 14:53:44 +0100

    Added online game functionality

--- app.js
+++ app.js
@@ -17,7 +17,67 @@ const settingsRoutes = require('./routes/settingsRoutes')
 const app = express();
 const server = http.createServer(app);
 
+// Sockets requires
+const socketIo = require("socket.io");
+const fs = require("fs");
+
+const io = socketIo(server);
+const clients = {};
+
+// Sockets End
+
 init()
+
+var players = {}; // opponent: scoket.id of the opponent, symbol = "X" | "O", socket: player's socket
+var unmatched;
+
+
+// When a client connects
+io.on("connection", function(socket) {
+    let id = socket.id;
+
+    console.log("New client connected. ID: ", socket.id);
+    clients[socket.id] = socket;
+
+    socket.on("disconnect", () => {// Bind event for that socket (player)
+        console.log("Client disconnected. ID: ", socket.id);
+        delete clients[socket.id];
+        socket.broadcast.emit("clientdisconnect", id);
+    });
+
+    join(socket); // Fill 'players' data structure
+
+    if (opponentOf(socket)) { // If the current player has an opponent the game can begin
+        socket.emit("game.begin", { // Send the game.begin event to the player
+            symbol: players[socket.id].symbol
+        });
+
+        opponentOf(socket).emit("game.begin", { // Send the game.begin event to the opponent
+            symbol: players[opponentOf(socket).id].symbol 
+        });
+    }
+
+
+    // Event for when any player makes a move
+    socket.on("make.move", function(data) {
+        if (!opponentOf(socket)) {
+            // This shouldn't be possible since if a player doens't have an opponent the game board is disabled
+            return;
+        }
+
+        // Validation of the moves can be done here
+
+        socket.emit("move.made", data); // Emit for the player who made the move
+        opponentOf(socket).emit("move.made", data); // Emit for the opponent
+    });
+
+    // Event to inform player that the opponent left
+    socket.on("disconnect", function() {
+        if (opponentOf(socket)) {
+        opponentOf(socket).emit("opponent.left");
+        }
+    });
+});
 
 function init() {
     app.use(bodyParser.urlencoded({extended: false}));
@@ -52,10 +112,39 @@ function routes() {
     app.use(userRoutes)
     app.use(chooseModeRoutes)
     app.use(settingsRoutes)
+    app.get("/online_game", (req, res) => {
+        const stream = fs.createReadStream(__dirname + "/views/onlineGame.html");
+        stream.pipe(res);
+    });
 }
 
 function startListening() {
     server.listen(3000, function(){
         console.log("server is listening on port: 3000\n");
     });
+}
+
+function join(socket) {
+    players[socket.id] = {
+        opponent: unmatched,
+        symbol: "X",
+        socket: socket
+    };
+
+    // If 'unmatched' is defined it contains the socket.id of the player who was waiting for an opponent
+    // then, the current socket is player #2
+    if (unmatched) { 
+        players[socket.id].symbol = "O";
+        players[unmatched].opponent = socket.id;
+        unmatched = null;
+    } else { //If 'unmatched' is not define it means the player (current socket) is waiting for an opponent (player #1)
+        unmatched = socket.id;
+    }
+}
+
+function opponentOf(socket) {
+    if (!players[socket.id].opponent) {
+        return;
+    }
+    return players[players[socket.id].opponent].socket;
 }
